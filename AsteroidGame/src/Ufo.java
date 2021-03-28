3d7c5200fa12d25afc3f16be83a0fd7ecccb2f93
Fazekas Albert <khhun12@gmail.com>
2021-03-28 22:32:04 +0200

    Create Ufo.java

---
+++ AsteroidGame/src/Ufo.java
@@ -0,0 +1,34 @@
+
+public class Ufo extends FlyingObject{
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean move() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onExplode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSolarStormCase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
