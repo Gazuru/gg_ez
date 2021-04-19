fe1170a2997d4712d5df534cb0db3efe45081e5a
Gonda Gábor <energyteamgazuru@gmail.com>
2021-04-19 12:39:08 +0200

    Update Testable.java

--- AsteroidGame/src/Testable.java
+++ AsteroidGame/src/Testable.java
@@ -3,11 +3,20 @@ public class Testable<X, Integer> {
     public X obj;
     public Integer num;
 
+    /**
+     * A teszteléshez szükséges elem tuple-be rendezését oldja meg, az x paraméter bármilyen objektum, míg az y az ID-je
+     * @param x teszteléshez hozzáadni kívánt objektum
+     * @param y az objektum azonosítási száma
+     */
     public Testable(X x, Integer y) {
         obj = x;
         num = y;
     }
 
+    /**
+     * Visszaadja a tárolt objektum osztályát
+     * @return obj objektum Class értéke
+     */
     public Class getObj(){
         return obj.getClass();
     }
